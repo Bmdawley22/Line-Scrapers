fbdad1a7297af3f008317c1e8b585df1730e88e0
Bmdawley22 <dawleyb11@gmail.com>
2023-10-24 13:58:32 -0500

    updated

--- DK_NFL_Lines_Parser.py
+++ DK_NFL_Lines_Parser.py
@@ -3,6 +3,8 @@ import math
 import time
 from bs4 import BeautifulSoup
 
+from scraperParameters import params
+
 url = 'https://sportsbook.draftkings.com/leagues/football/nfl'
 response = requests.get(url)
 
@@ -10,12 +12,21 @@ header = {
     'authorization': "Nzk2MDgxNzg5NTU4NDU2MzUx.GgQdGC.ZCqoFX8tlo7RNB5UgZaLJuuMqh7PvaaX-m1fyo"
 }
 
-
 reqCount = 0
 prevTeams = []
 prevLines = []
+numLineMoves = 0
 
-while reqCount < 120:
+
+def sendNotificationToDiscord(msg):
+    print("\n******LINE CHANGE********")
+    print(msg)
+    print("*************************")
+    r = requests.post(f'https://discord.com/api/v9/channels/{params["CFBChannelID"]}/messages',
+                      data={'content': msg}, headers=header)
+
+
+while reqCount < (params['maxRunTimeInMin'] * (60 / params['repRateInS'])):
 
     print('\nParsing DK NFL Spreads data...')
 
@@ -32,24 +43,35 @@ while reqCount < 120:
 
     if reqCount > 0:
         print('Comparing previous odds...\n')
-        for i in range(1):
-            if lines[math.floor(4*i)].text != prevLines[math.floor(4*i)].text:
-                msg = f'Game {i+1}: {prevTeams[math.floor(2*i)].text}({prevLines[math.floor(4*i)].text}) to ({lines[math.floor(4*i)].text})'
-                print("\n******LINE CHANGE********")
-                print(msg)
-                print("*************************")
-                r = requests.post("https://discord.com/api/v9/channels/1166388909618511894/messages",
-                                  data={'content': msg}, headers=header)
+        for i in range(numGames):
+            if params['test'] == True:
+                print(
+                    f'{i}: {teams[2*i].text}({lines[4*i].text})({lines[4*i+1].text}) || ({prevLines[4*i].text})({prevLines[4*i+1].text})')
+            if lines[4*i].text != prevLines[4*i].text:
+                msg = f'Line Move: {prevTeams[2*i].text}({prevLines[4*i].text}) to ({lines[4*i].text})'
+                sendNotificationToDiscord(msg)
+                numLineMoves = numLineMoves + 1
+
+            elif lines[4*i+1].text != prevLines[4*i+1].text:
+                msg = f'Total Move: {prevTeams[2*i].text}({prevLines[4*i+1].text}) to ({lines[4*i+1].text})'
+                sendNotificationToDiscord(msg)
+                numLineMoves = numLineMoves + 1
 
     prevTeams = teams
     prevLines = lines
 
+    if params['test'] == True:
+        if reqCount == 0:
+            prevLines[0] = BeautifulSoup(
+                '<span class="sportsbook-outcome-cell__line">+Test</span>', 'html.parser')
+
     if reqCount > 0:
-        print('No line changes - will check again in 1 min.')
+        print('\nNo line changes - will check again in 1 min.')
+        print(f'\nNumber of Line Moves: {numLineMoves}')
         print(f'Runtime: {60*reqCount} seconds')
     else:
         print('Will check for line changes in 1 min.')
 
     reqCount = reqCount + 1
 
-    time.sleep(60)
+    time.sleep(params['repRateInS'])
---
+++ scraperParameters.py
@@ -0,0 +1,2 @@
+params = {'test': True, 'repRateInS': 10, 'maxRunTimeInMin': 120,
+          'NFLChannelID': '1166388909618511894', 'CFBChannelID': '1166410287453319250'}

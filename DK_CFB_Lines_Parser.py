9c41389ee38b6c14a1a2ad2805667fcb76fc56e0
Bmdawley22 <dawleyb11@gmail.com>
2023-10-25 15:24:08 -0500

    small changes

--- DK_CFB_Lines_Parser.py
+++ DK_CFB_Lines_Parser.py
@@ -44,12 +44,12 @@ while reqCount < (params['maxRunTimeInMin'] * (60 / params['repRateInS'])):
                 print(
                     f'{i}: {teams[2*i].text}({lines[4*i].text})({lines[4*i+1].text}) || ({prevLines[4*i].text})({prevLines[4*i+1].text})')
             if lines[4*i].text != prevLines[4*i].text and teams[2*i].text == prevTeams[2*i].text:
-                msg = f'Line Move: {prevTeams[2*i].text}:({prevLines[4*i].text}) to ({lines[4*i].text})'
+                msg = f'Line Move: {prevTeams[2*i].text}: ({prevLines[4*i].text}) to ({lines[4*i].text})'
                 sendNotificationToDiscord(msg)
                 numLineMoves = numLineMoves + 1
 
             elif lines[4*i+1].text != prevLines[4*i+1].text and teams[2*i].text == prevTeams[2*i].text:
-                msg = f'Total Move: {prevTeams[2*i].text}:({prevLines[4*i+1].text}) to ({lines[4*i+1].text})'
+                msg = f'Total Move: {prevTeams[2*i].text}: ({prevLines[4*i+1].text}) to ({lines[4*i+1].text})'
                 sendNotificationToDiscord(msg)
                 numLineMoves = numLineMoves + 1
 

1056df20f7670a508be602ac4a48ed946794da80
Bmdawley22 <dawleyb11@gmail.com>
2023-10-27 11:58:34 -0500

    removed each sport scraper file

--- scraperParameters.py
+++
@@ -1,2 +0,0 @@
-params = {'test': True, 'repRateInS': 10, 'maxRunTimeInMin': 120,
-          'NFLChannelID': '1166388909618511894', 'CFBChannelID': '1166410287453319250', 'NBAChannelID': '1166809465841000568', 'pass': ''}
